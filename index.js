3b79f3d5352a3a3f7f6793fb14985e88d775e356
Taavetti Kukkonen <taavetti.kukkonen@taitounited.fi>
2021-07-23 14:08:02 +0300

    fix: unused parameters being generated

--- index.js
+++ index.js
@@ -11,15 +11,20 @@ const del = require('del');
 
 program
   .option('--schemaFilePath [value]', 'path of your graphql schema file')
-  .option('--destDirPath [value]', 'dir you want to store the generated queries')
+  .option(
+    '--destDirPath [value]',
+    'dir you want to store the generated queries',
+  )
   .parse(process.argv);
 
 const schemaFilePath = program.schemaFilePath;
 const destDirPath = program.destDirPath;
 
-console.log('[gqlg]:', `Going to create 3 folders to store the queries inside path: ${destDirPath}`);
+console.log(
+  '[gqlg]:',
+  `Going to create 3 folders to store the queries inside path: ${destDirPath}`,
+);
 const typeDef = fs.readFileSync(schemaFilePath);
-
 
 const source = new Source(typeDef);
 // const ast = parse(source);
@@ -35,7 +40,6 @@ const addQueryDepthLimit = 100;
 function cleanName(name) {
   return name.replace(/[\[\]!]/g, '');
 }
-
 
 /**
  * Generate the query for the specified field
@@ -81,12 +85,12 @@ function generateQuery(name, parentType) {
   return { query, meta };
 
   /**
-     * Generate the query for the specified field
-     * @param name name of the current field
-     * @param parentType parent type of the current field
-     * @param parentFields preceding parent field and type combinations
-     * @param level current depth level of the current field
-     */
+   * Generate the query for the specified field
+   * @param name name of the current field
+   * @param parentType parent type of the current field
+   * @param parentFields preceding parent field and type combinations
+   * @param level current depth level of the current field
+   */
   function generateFieldData(name, parentType, parentFields, level) {
     // console.log('Generating query for ', name, parentType);
 
@@ -99,6 +103,21 @@ function generateQuery(name, parentType) {
 
     // Start the query with the field name
     let fieldStr = ' '.repeat(level * tabSize) + field.name;
+
+    // Retrieve the current field type
+    const curTypeName = cleanName(field.type.inspect());
+    const curType = gqlSchema.getType(curTypeName);
+
+    // Don't add a field if it has been added in the query already.
+    // This happens when there is a recursive field
+    if (parentFields.filter(x => x.type === curTypeName).length) {
+      return { query: '', meta: {} };
+    }
+
+    // Stop adding new fields once the specified level depth limit is reached
+    if (level >= addQueryDepthLimit) {
+      return { query: '', meta: {} };
+    }
 
     // If the field has arguments, add them
     if (field.args && field.args.length) {
@@ -116,21 +135,6 @@ function generateQuery(name, parentType) {
           type: arg.type,
         });
       });
-    }
-
-    // Retrieve the current field type
-    const curTypeName = cleanName(field.type.inspect());
-    const curType = gqlSchema.getType(curTypeName);
-
-    // Don't add a field if it has been added in the query already.
-    // This happens when there is a recursive field
-    if (parentFields.filter(x => x.type === curTypeName).length) {
-      return { query: '', meta: {} };
-    }
-
-    // Stop adding new fields once the specified level depth limit is reached
-    if (level >= addQueryDepthLimit) {
-      return { query: '', meta: {} };
     }
 
     // Get all the fields of the field type, if available
@@ -202,11 +206,16 @@ let indexJsExportAll = '';
 
 if (gqlSchema.getMutationType()) {
   let mutationsIndexJs = indexJsStart;
-  Object.keys(gqlSchema.getMutationType().getFields()).forEach((mutationType) => {
-    const { query } = generateQuery(mutationType, 'Mutation');
-    fs.writeFileSync(path.join(mutationsFolder, `./${mutationType}.gql`), query);
-    mutationsIndexJs += `module.exports.${mutationType} = fs.readFileSync(path.join(__dirname, '${mutationType}.gql'), 'utf8');\n`;
-  });
+  Object.keys(gqlSchema.getMutationType().getFields()).forEach(
+    (mutationType) => {
+      const { query } = generateQuery(mutationType, 'Mutation');
+      fs.writeFileSync(
+        path.join(mutationsFolder, `./${mutationType}.gql`),
+        query,
+      );
+      mutationsIndexJs += `module.exports.${mutationType} = fs.readFileSync(path.join(__dirname, '${mutationType}.gql'), 'utf8');\n`;
+    },
+  );
   fs.writeFileSync(path.join(mutationsFolder, 'index.js'), mutationsIndexJs);
   indexJsExportAll += `module.exports.mutations = require('./mutations');
 `;
@@ -229,12 +238,20 @@ if (gqlSchema.getQueryType()) {
 
 if (gqlSchema.getSubscriptionType()) {
   let subscriptionsIndexJs = indexJsStart;
-  Object.keys(gqlSchema.getSubscriptionType().getFields()).forEach((subscriptionType) => {
-    const { query } = generateQuery(subscriptionType, 'Subscription');
-    fs.writeFileSync(path.join(subscriptionsFolder, `./${subscriptionType}.gql`), query);
-    subscriptionsIndexJs += `module.exports.${subscriptionType} = fs.readFileSync(path.join(__dirname, '${subscriptionType}.gql'), 'utf8');\n`;
-  });
-  fs.writeFileSync(path.join(subscriptionsFolder, 'index.js'), subscriptionsIndexJs);
+  Object.keys(gqlSchema.getSubscriptionType().getFields()).forEach(
+    (subscriptionType) => {
+      const { query } = generateQuery(subscriptionType, 'Subscription');
+      fs.writeFileSync(
+        path.join(subscriptionsFolder, `./${subscriptionType}.gql`),
+        query,
+      );
+      subscriptionsIndexJs += `module.exports.${subscriptionType} = fs.readFileSync(path.join(__dirname, '${subscriptionType}.gql'), 'utf8');\n`;
+    },
+  );
+  fs.writeFileSync(
+    path.join(subscriptionsFolder, 'index.js'),
+    subscriptionsIndexJs,
+  );
   indexJsExportAll += 'module.exports.subscriptions = require(\'./subscriptions\');';
 } else {
   console.log('[gqlg warning]:', 'No subscription type found in your schema');
